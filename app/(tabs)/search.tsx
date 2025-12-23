3c2c99767a9699f60201c41a120fb10dd3ceca47
Aalvee Ahtav <aalvee666@gmail.com>
2025-12-23 05:09:57 -0600

    04: database and trending setup

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,21 +1,23 @@
-import { View, Text, Image, FlatList, ActivityIndicator } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { images } from '@/constants/images'
 import MovieCard from '@/components/MovieCard'
-import { fetchMovies } from '@/services/api'
-import useFetch from '@/services/useFetch'
-import { icons } from '@/constants/icons'
 import SearchBar from '@/components/SearchBar'
+import { icons } from '@/constants/icons'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api'
+import { updateSearchCount } from '@/services/appwrite'
+import useFetch from '@/services/useFetch'
+import React, { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, Image, Text, View } from 'react-native'
 
 const search = () => {
   const [searchQuery, setSearchQuery] = useState('')
 
-  const { data: movies, loading, error, refetch:loadMovies, reset } = useFetch(() => fetchMovies({
-     query: searchQuery
+  const { data: movies, loading, error, refetch: loadMovies, reset } = useFetch(() => fetchMovies({
+    query: searchQuery
   }), false)
 
   useEffect(() => {
-    const timeoutId = setTimeout( async () => {
+
+    const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await loadMovies();
       } else {
@@ -24,15 +26,20 @@ const search = () => {
     }, 500);
 
     return () => clearTimeout(timeoutId);
-
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (movies?.length > 0 && movies?.[0]) {
+      updateSearchCount(searchQuery, movies[0]);
+    }
+  }, [movies]);
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source ={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
+      <Image source={images.bg} className="flex-1 absolute w-full z-0" resizeMode="cover" />
 
-      <FlatList 
-        data={movies} 
+      <FlatList
+        data={movies}
         renderItem={({ item }) => <MovieCard {...item} />}
         keyExtractor={(item) => item.id.toString()}
         className="px-5"
@@ -42,20 +49,20 @@ const search = () => {
           gap: 16,
           marginVertical: 16,
         }}
-        contentContainerStyle={{paddingBottom: 100}}
+        contentContainerStyle={{ paddingBottom: 100 }}
 
         ListHeaderComponent={
           <>
             <View className="w-full flex-row justify-center mt-20 items-center">
-              <Image source = {icons.logo} className="w-12 h-10" />
+              <Image source={icons.logo} className="w-12 h-10" />
             </View>
 
             <View className="my-5">
-               <SearchBar 
-                  placeholder="Search for a movie"
-                  value={searchQuery}
-                  onChangeText={(text: string) => setSearchQuery(text)}
-               />
+              <SearchBar
+                placeholder="Search for a movie"
+                value={searchQuery}
+                onChangeText={(text: string) => setSearchQuery(text)}
+              />
             </View>
 
             {loading && (
@@ -72,7 +79,7 @@ const search = () => {
                   Search Results for{' '}
                   <Text className="text-accent">{searchQuery}</Text>
                 </Text>
-            )}
+              )}
           </>
         }
 
@@ -81,7 +88,7 @@ const search = () => {
             <View className="mt-10 px-5">
               <Text className="text-gray-500 text-center">
                 {searchQuery.trim() ? 'No results found.' : 'Start typing to search for movies'}
-              </Text> 
+              </Text>
             </View>
           ) : null
         }
